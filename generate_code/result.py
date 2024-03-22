271874eced68eff2838479443bf2c0ddda11d49f
Marta Herod <marta.herod@PAYBACK.net>
2024-03-22 21:54:29 +0100

    testy

--- generate_code/result.py
+++ generate_code/result.py
@@ -1,14 +1,11 @@
 def find_common_divisors(a, b):
-    divisors_a = [i for i in range(1, a + 1) if a % i == 0]
-    divisors_b = [i for i in range(1, b + 1) if b % i == 0]
-    common_divisors = [i for i in divisors_a if i in divisors_b]
-    return common_divisors
+    return [i for i in range(1, min(a, b) + 1) if a % i == 0 and b % i == 0]
 
 def generate_primes(n):
     primes = []
     for num in range(2, n+1):
         is_prime = True
-        for i in range(2, int(num ** 0.5) + 1):
+        for i in range(2, int(num**0.5) + 1):
             if num % i == 0:
                 is_prime = False
                 break
